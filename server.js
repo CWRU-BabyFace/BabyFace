7866e2bdd9c1f006bcac57d87dd3fe38d97ef13f
Brooke <Duckiebeewarren@gmail.com>
2017-11-02 15:16:25 -0400

    updating server file

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var db = require("./models");
+//Trying to reference all the routing files in the controller folder
+//Then allow handlebars to be used in those files 
+//var routes = require("./controllers");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+//app.use("/", routes); ??????
+
+// Setting up Express middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("public"));
+
+require("./controllers/child-api.js")(app);
+require("./controllers/memory-api.js")(app);
+require("./controllers/user-api.js")(app);
+require("./controllers/html-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
