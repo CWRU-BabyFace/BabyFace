dc00580f85b58dafd307f10b1eb1e85b4bc190c7
mrengel <m.r.engel2010@gmail.com>
2017-11-03 23:26:34 -0400

    Models are working, creating tables in mySql

--- server.js
+++ server.js
@@ -20,12 +20,13 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Static directory
-app.use(express.static("public"));
+app.use(express.static(__dirname + "./public"));
+
 
 require("./controllers/child-api.js")(app);
 require("./controllers/memory-api.js")(app);
 require("./controllers/user-api.js")(app);
-require("./controllers/html-routes.js")(app);
+
 
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync({ force: false }).then(function() {
